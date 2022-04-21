2e45094d3680a7faf1b4b4e5314cd4017af882e5
Rodrigo Diaz <rodrigohdiaz@gmail.com>
2022-04-21 17:33:57 +1200

    set up background

---
+++ src/script.js
@@ -0,0 +1,74 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas 
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+/**
+ * Objects
+ */
+const material = new THREE.MeshNormalMaterial()
+// material.color= new THREE.Color(0x598885)
+material.wireframe = true
+material.side = THREE.DoubleSide
+
+const sphere = new THREE.Mesh(
+new THREE.SphereGeometry(1, 32, 32),
+material
+)
+
+sphere.geometry.setAttribute(
+    'uv2',
+    new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
+)
+sphere.rotation.x = 300
+sphere.position.z = 3
+sphere.position.x = 0.8
+
+scene.add(sphere)
+
+
+// Camera
+const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+// Animate
+const clock = new THREE.Clock()
+
+
+/**
+ * Animate 
+ */
+const tick = () =>
+{
+    // Clock
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
